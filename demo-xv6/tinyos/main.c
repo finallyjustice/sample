749a069726fe759a6f519ce00c507ba72011aa6c
Dongli Zhang <dongli.zhang0129@gmail.com>
2016-08-02 10:34:26 +0800

    tinyos

---
+++ demo-xv6/tinyos/main.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "x86.h"
+
+int main(void)
+{
+	cprintf("Hello World!\n");
+	while(1);
+	return 0;
+}
